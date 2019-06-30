e4e05f45d88259db0e590c2c85e93809c5275173
LORRAINE BAYNOSA <you@example.com>
2019-06-29 17:37:07 -0700

    saving changes

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -21,63 +21,99 @@ var db = require("../models");
 // });
 
 // USING SEQUELIZE ACTIVITY for router.get:
-module.exports = function(app) {
-
-  // GET route for getting all of the posts
-  app.get("/api/burgers/", function(req, res) {
+module.exports = function (app) {
+  // GET route for main index.handlebars
+  app.get("/api/burgers/", function (req, res) {
+    res.render("index");
+  });
+  // GET route for getting all of the burgers
+  app.get("/api/burgers/", function (req, res) {
     db.Burger.findAll({})
-      .then(function(dbBurger) {
+      .then(function (dbBurger) {
         var hbsObject = {
-                burgers: dbBurger
-              };
-              console.log(hbsObject);
-              res.render("index", hbsObject);
-
+          burgers: dbBurger
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
         // res.json(dbBurger);
       });
   });
+  // router.post("/api/burgers", function(req, res) {
+  //   Burger.create([
+  //     "burger_name", "devoured"
+  //   ], [
+  //     req.body.burger_name, req.body.devoured
+  //   ], function(result) {
+  //     // Send back the ID of the new burger
+  //     res.json({ id: result.insertId });
+  //   });
+  // });
+  // // where "burger_name", "devoured" refer to columns within the database db
+
+  // USING SEQUELIZE ACTIVITY for router.post:
+  // POST route for saving a new burger
+  app.post("/api/burgers", function (req, res) {
+    console.log(req.body);
+    db.Burger.create({
+      burger_name: req.body.burger_name,
+      devoured: req.body.devoured,
+    })
+      .then(function (dbBurger) {
+        res.json(dbBurger);
+      });
+  });
+  // router.put("/api/burgers/:id", function(req, res) {
+  //   var condition = "id = " + req.params.id;
+
+  //   console.log("condition", condition);
+
+  //   Burger.update({
+  //     devoured: req.body.devoured
+  //   }, condition, function(result) {
+  //     if (result.changedRows == 0) {
+  //       // If no rows were changed, then the ID must not exist, so 404
+  //       return res.status(404).end();
+  //     } else {
+  //       res.status(200).end();
+  //     }
+  //   });
+  // });
+  // USING SEQUELIZE ACTIVITY for router.put:
+  app.put("/api/burgers", function (req, res) {
+    db.Burger.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      .then(function (dbBurger) {
+        res.json(dbBurger);
+      });
+  });
+  // router.delete("/api/burgers/:id", function(req, res) {
+  //   var condition = "id = " + req.params.id;
+
+  //   Burger.destroy(condition, function(result) {
+  //     if (result.affectedRows == 0) {
+  //       // If no rows were changed, then the ID must not exist, so 404
+  //       return res.status(404).end();
+  //     } else {
+  //       res.status(200).end();
+  //     }
+  //   });
+  // });
+  // USING SEQUELIZE ACTIVITY for router.delete:
+  app.delete("/api/burgers/:id", function (req, res) {
+    db.Burger.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function (dbBurger) {
+        res.json(dbBurger);
+      });
+  });
 };
-// router.post("/api/burgers", function(req, res) {
-//   Burger.create([
-//     "burger_name", "devoured"
-//   ], [
-//     req.body.burger_name, req.body.devoured
-//   ], function(result) {
-//     // Send back the ID of the new burger
-//     res.json({ id: result.insertId });
-//   });
-// });
-// // where "burger_name", "devoured" refer to columns within the database db
-
-// router.put("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   console.log("condition", condition);
-
-//   Burger.update({
-//     devoured: req.body.devoured
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
-// router.delete("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   Burger.destroy(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
 
 // // Export routes for server.js to use.
 // module.exports = router;
